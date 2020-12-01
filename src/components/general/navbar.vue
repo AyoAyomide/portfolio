e6e0b1ddd9b5e8af6741240b4e387e46303914f1
AyoAyomide <ayoayomide@gmail.com>
2020-12-01 15:05:19 +0100

    update for side bar

--- src/components/general/navbar.vue
+++ src/components/general/navbar.vue
@@ -1,29 +1,106 @@
 <template>
   <div class="demo-container">
     <!-- Drawer -->
-    <ui-drawer  type="modal" nav-id="drawer">
-      <ui-drawer-header>
-        <ui-drawer-title>Title</ui-drawer-title>
-        <ui-drawer-subtitle>Subtitle</ui-drawer-subtitle>
+    <ui-drawer type="modal" nav-id="drawer">
+      <ui-drawer-header class="nav-header">
+        <div class="img-wraper">
+          <img src="../../assets/logo.png" />
+        </div>
+        <h4 :class="$tt('headline5')"><b>AyoAyomide</b></h4>
       </ui-drawer-header>
       <ui-drawer-content>
-        <ui-nav>
-          <ui-nav-item href="javascript:void(0)" active
-            >Item {{ 0 }}</ui-nav-item
+        <!-- nav content wrap-->
+        <ui-list :type="2">
+          <!-- nav content-->
+          <ui-item
+            class="list_wrap"
+            v-for="(item, index) in nav_content"
+            :key="index"
           >
-          <ui-nav-item v-for="i in 12" :key="i" href="javascript:void(0)"
-            >Item {{ i }}</ui-nav-item
-          >
-        </ui-nav>
+            <ui-item-first-content :style="{ backgroundColor: item.theme }">
+              <ui-icon :style="{ color: item.color }">{{ item.icon }}</ui-icon>
+            </ui-item-first-content>
+            <ui-item-text-content>
+              <ui-item-text1>{{ item.title }}</ui-item-text1>
+              <ui-item-text2>{{
+                item.subtitle
+              }}</ui-item-text2></ui-item-text-content
+            >
+          </ui-item>
+        </ui-list>
       </ui-drawer-content>
+      <div class="footer"></div>
     </ui-drawer>
     <ui-drawer-backdrop></ui-drawer-backdrop>
   </div>
 </template>
 <script>
+// import uiList from "@/components/sharp/icon_list.vue";
 export default {
   name: "navbar",
+  components: {
+    // uiList,
+  },
+  data() {
+    return {
+      nav_content: [
+        {
+          title: "Web",
+          subtitle: "hello bla bla",
+          icon: "star",
+          theme: "#D5C8FF",
+          color: "#7958EB",
+        },
+        {
+          title: "Web",
+          subtitle: "hello bla bla",
+          icon: "star",
+          theme: "#C6FCFC",
+          color: "#00A5A5",
+        },
+      ],
+    };
+  },
 };
 </script>
 <style scoped lang="scss">
+.nav-header {
+  text-align: center;
+  .img-wraper {
+    @include dev(2px, $primary-col);
+    display: flex;
+    padding: 1em;
+    height: 10em;
+    width: 10em;
+    @include round;
+    margin: auto;
+    margin-top: 2em;
+    margin-bottom: 1em;
+    img {
+      margin: auto;
+      width: 8em;
+      @include round;
+      vertical-align: center;
+    }
+  }
+}
+.list_wrap {
+  // @include dev;
+  .mdc-list-item__graphic {
+    @include round;
+    height: 3em;
+    width: 3em;
+    margin-right: 10px;
+  }
+}
+.demo-container {
+  .footer {
+    @include dev;
+    height: 3em;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+  }
+}
 </style>
---
+++ src/components/sharp/icon_list.vue
@@ -0,0 +1,11 @@
+<template>
+  <ui-item>
+    <ui-item-first-content>
+      <ui-icon>star</ui-icon>
+    </ui-item-first-content>
+    <ui-item-text-content>
+      <ui-item-text1>Web</ui-item-text1>
+      <ui-item-text2>Web project</ui-item-text2></ui-item-text-content
+    >
+  </ui-item>
+</template>
