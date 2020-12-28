e0575556ac2cdc61f6199890fa458d3743ad869f
AyoAyomide <ayoayomide@gmail.com>
2020-12-28 12:55:13 +0100

    manifest and pwa fix

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,13 @@ module.exports = {
     },
 
     pwa: {
-      name: 'AyoAyomide'
+        manifestPath: 'manifest.json',
+        iconPaths: {
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+            // maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        },
     }
 }
